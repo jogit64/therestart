9603e7136f52fe26de4d13c40b3553368c0354c9
jogit64 <usersfdc18@gmail.com>
2023-06-25 23:51:08 +0200

    Version diminuée mais - apk qui se lance enfin - les swip fonctionnent - question un niveau de stack un necessary ?!

--- App.tsx
+++ App.tsx
@@ -1,41 +1,42 @@
 import React, { useEffect } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
+//import { useFonts } from "expo-font";
+//import * as SplashScreen from "expo-splash-screen";
 
-import UserContext, { UserProvider } from "./utils/UserContext";
+//import UserContext, { UserProvider } from "./utils/UserContext";
+import { UserProvider } from "./utils/UserContext";
 
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./AppNavigator";
 
-import LoadingSpinner from "./utils/LoadingSpinner";
+//import LoadingSpinner from "./utils/LoadingSpinner";
 
 export default function App() {
   // Charger les polices personnalisées
-  const [fontsLoaded] = useFonts({
-    lemon: require("./assets/fonts/lemon-regular.ttf"),
-    roboto: require("./assets/fonts/roboto-regular.ttf"),
-    roboto700: require("./assets/fonts/roboto-700.ttf"),
-    roboto500: require("./assets/fonts/roboto-500.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   lemon: require("./assets/fonts/lemon-regular.ttf"),
+  //   roboto: require("./assets/fonts/roboto-regular.ttf"),
+  //   roboto700: require("./assets/fonts/roboto-700.ttf"),
+  //   roboto500: require("./assets/fonts/roboto-500.ttf"),
+  // });
 
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
+  // useEffect(() => {
+  //   async function prepare() {
+  //     await SplashScreen.preventAutoHideAsync();
+  //   }
 
-    prepare();
-  }, []);
+  //   prepare();
+  // }, []);
 
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+  // useEffect(() => {
+  //   if (fontsLoaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
 
   // Si les polices ne sont pas chargées, afficher le composant de chargement
-  if (!fontsLoaded) {
-    return <LoadingSpinner />;
-  }
+  // if (!fontsLoaded) {
+  //   return <LoadingSpinner />;
+  // }
 
   return (
     // Fournir le contexte utilisateur à tous les composants enfants
--- src/unauthenticated/screens/AccueilScreen.tsx
+++ src/unauthenticated/screens/AccueilScreen.tsx
@@ -1,128 +1,29 @@
 import React from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-//import { LinearGradient } from "expo-linear-gradient";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-//import { StatusBar } from "expo-status-bar";
-import Swiper from "react-native-swiper";
-
-// import MaterialButtonViolet1 from "./accueil/bottompart/MaterialButtonViolet1";
-// import MaterialButtonViolet3 from "./accueil/bottompart/MaterialButtonViolet3";
-
-import Swiper1 from "./accueil/swiper1/swiper1";
-import Swiper2 from "./accueil/swiper2/swiper2";
-import Swiper3 from "./accueil/swiper3/swiper3";
-
-import { RootStackParamList } from "../../../utils/navigationTypes";
-
-// Define color constants
-const COLORS = {
-  dot: "#dcdeeb",
-  activeDot: "#6f78bd",
-  gradientStart: "#e9f6ff",
-  gradientEnd: "#f8fcff",
-  button1Bg: "rgba(111,120,189,1)",
-  button2Bg: "rgba(255,255,255,1)",
-  button2Shadow: "#000",
-  statusBarBg: "rgba(111,120,189,1)",
-};
-
-export default function AccueilScreen() {
-  const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, "Accueil">>();
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <View style={{ flex: 3 }}>
-        <Swiper
-          showsButtons={false}
-          showsPagination={true}
-          dotColor={COLORS.dot}
-          activeDotColor={COLORS.activeDot}
-          loop={false}
-        >
-          <Swiper1 />
-
-          <Swiper2 />
-
-          <Swiper3 />
-        </Swiper>
-      </View>
-
-      {/* Button area */}
-      <View style={{ flex: 1 }}>
-        {/* <StatusBar style="light" backgroundColor={COLORS.statusBarBg} />
-        <MaterialButtonViolet1
-          style={styles.materialButtonViolet1}
-          onPress={() => navigation.navigate("SignUp")}
-        ></MaterialButtonViolet1>
-        <MaterialButtonViolet3
-          style={styles.materialButtonViolet3}
-          onPress={() => navigation.navigate("Login")}
-        ></MaterialButtonViolet3> */}
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("SignUp")}
-        >
-          <Text style={styles.buttonText}>S'inscrire</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("Login")}
-        >
-          <Text style={styles.buttonText}>Me connecter</Text>
-        </TouchableOpacity>
-      </View>
+      <Text style={styles.text}>
+        Hello World depuis accueilscreen appelé depuis l'appnavigatoravec bottom
+        actif
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  // materialButtonViolet1: {
-  //   height: 55,
-  //   width: 312,
-  //   backgroundColor: COLORS.button1Bg,
-  //   borderRadius: 11,
-  //   marginTop: 5,
-  //   alignSelf: "center",
-  // },
-  // materialButtonViolet3: {
-  //   height: 55,
-  //   width: 312,
-  //   backgroundColor: COLORS.button2Bg,
-  //   shadowColor: COLORS.button2Shadow,
-  //   shadowOffset: {
-  //     width: 3,
-  //     height: 3,
-  //   },
-  //   elevation: 5,
-  //   shadowOpacity: 1,
-  //   shadowRadius: 0,
-  //   borderRadius: 11,
-  //   marginTop: 21,
-  //   alignSelf: "center",
-  // },
-  page: {
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
-    height: "100%",
+    backgroundColor: "#F5FCFF",
   },
-
-  button: {
-    backgroundColor: "#4287f5",
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  buttonText: {
-    color: "#ffffff",
+  text: {
+    fontSize: 20,
     textAlign: "center",
+    margin: 10,
   },
 });
+
+export default App;
